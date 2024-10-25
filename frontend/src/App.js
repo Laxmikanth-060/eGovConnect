5d21bb92ac15bc56df5f131e08b2d739df206e90
Laxmikanth-060 <laxmikanthanubothula@gmail.com>
2024-10-25 15:37:50 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Services from "./components/Services/Services";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <RouterProvider router={appRouter}>
+      <App />
+    </RouterProvider>
+);
